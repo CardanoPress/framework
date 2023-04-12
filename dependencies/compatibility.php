e9a51159237227bcc2904360586a4b4c9a2d7b65
kermage <genealyson.torcende@gmail.com>
2023-04-12 15:37:07 +0800

    remove unnecessary function

--- dependencies/compatibility.php
+++ dependencies/compatibility.php
@@ -5,10 +5,6 @@
  * @since   2.0.0
  */
 
-function cardanopress_framework_version(): string {
-    return '2.0.0';
-}
-
 if (! interface_exists('Psr\Log\LoggerInterface')) {
     class_alias('CardanoPress\Dependencies\Psr\Log\LoggerInterface', 'Psr\Log\LoggerInterface');
 } else {
